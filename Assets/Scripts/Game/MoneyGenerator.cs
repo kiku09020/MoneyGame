996500323391095e52f592e2d98a380aab6576bc
504-11\cre <2102020004sk@edu.nca.ac.jp>
2023-04-11 17:02:40 +0900

    Unitask追加、生成＋移動仮作成

--- Assets/Scripts/Game/MoneyGenerator.cs
+++ Assets/Scripts/Game/MoneyGenerator.cs
@@ -4,9 +4,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using System.Threading;
 
 public class MoneyGenerator : MonoBehaviour
 {
+	[SerializeField] int waitFrame = 10;
+	[SerializeField] float moveToGroupDuration = 0.1f;
+	[SerializeField] float alignmentMoveDuration = 0.1f;
+
+
 	[Serializable]
 	class MoneyUnit {
 		public string name;
@@ -23,9 +30,16 @@ public class MoneyGenerator : MonoBehaviour
 
     [SerializeField] List<MoneyUnit> moneyUnitList = new List<MoneyUnit>();
 
-	//--------------------------------------------------
+	CancellationToken token;
 
-	private void OnValidate()
+    //--------------------------------------------------
+
+    private void Awake()
+    {
+		token = this.GetCancellationTokenOnDestroy();
+    }
+
+    private void OnValidate()
 	{
 		foreach(var moneyUnit in moneyUnitList) {
 			if (moneyUnit.Money != null && moneyUnit.Money.Data != null) {
@@ -37,11 +51,15 @@ public class MoneyGenerator : MonoBehaviour
 	/// <summary>
 	/// ê∂ê¨å„Ç…à⁄ìÆÇ≥ÇπÇÈ
 	/// </summary>
-	public void GenerateAndMove()
+	public async void GenerateAndMove()
 	{
-		var moneyObjList = Generate();
+		await UniTask.Delay(1, cancellationToken: token);
 
-		MoveToPlayerMG(moneyObjList);
+		if (UIManager.GetUIGroup<GameUIGroup>().gameObject.activeSelf) {
+			var moneyObjList = Generate();
+
+			MoveToPlayerMG(moneyObjList);
+		}
 	}
 
 	// ê∂ê¨
@@ -64,9 +82,35 @@ public class MoneyGenerator : MonoBehaviour
 		var count = 0;
 		foreach (var moneyUnit in moneyUnitList) {
 			for (int j = 0; j < moneyUnit.Money.Data.GeneratedCount; j++) {
-				moneyObjList[count].RectTransform.DOAnchorPos(moneyUnit.TargetPlayerMG.RectTransform.anchoredPosition, 1);
+
+				moneyObjList[count].RectTransform.DOLocalMove(moneyUnit.TargetPlayerMG.RectTransform.localPosition, moveToGroupDuration)
+					.OnComplete(() => MoveCompleted(moneyObjList));
+
 				count++;
 			}
 		}
 	}
+
+	async void MoveCompleted(List<Money> moneyObjList)
+	{
+		var count = 0;
+
+		foreach (var moneyUnit in moneyUnitList) {
+			for (int i = 0; i < moneyUnit.Money.Data.GeneratedCount; i++) {
+				var prevPos = moneyObjList[count].RectTransform.position;
+
+
+				moneyObjList[count].transform.SetParent(moneyUnit.TargetPlayerMG.transform);                 // à⁄ìÆå„Ç…êeÇ…éwíËÇ∑ÇÈ
+				var targetPos = moneyObjList[count].RectTransform.position;
+
+				moneyObjList[count].RectTransform.position = prevPos;
+
+				await UniTask.DelayFrame(waitFrame, cancellationToken: token);
+
+				moneyObjList[count].RectTransform.DOLocalMove(-targetPos, alignmentMoveDuration);
+
+				count++;
+			}
+		}
+    }
 }
